6ce28c5c292d804d75dd62a904d3bb56e5938c27
Himanshu Dorbi <105678703+HimanshuDorbi@users.noreply.github.com>
2022-06-08 01:02:42 +0530

    Time: 8 ms (45.56%), Space: 7.6 MB (100.00%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
-        int result = 0;
-        for (const auto& account : accounts) {
-            int total = accumulate(cbegin(account), cend(account), 0);
-            result = max(result, total);
+     
+      int i,j,m=accounts.size(),n=accounts[0].size(),sum2=0;
+        for(i=0;i<m;i++)
+        {
+            int sum=0;
+            for(j=0;j<n;j++)
+            {
+                sum+=accounts[i][j];
+            }
+            sum2=max(sum2,sum);   
         }
-        return result;
+        return sum2;
     }
+        
+        
 };
